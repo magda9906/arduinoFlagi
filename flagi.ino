05e82521af55bbc47a86120558f6ea00cc241269
Natalia Frankiewicz <n.frankiewicz@gmail.com>
2021-12-13 17:27:47 +0100

    fix

--- flagi.ino
+++ flagi.ino
@@ -1,10 +1,10 @@
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
 // Which pin on the Arduino is connected to the NeoPixels?
-#define PIN        3 
+#define PIN 3
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS 54 // Popular NeoPixel ring size
@@ -17,7 +17,8 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 #define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
 
-void setup() {
+void setup()
+{
   // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
   // Any other board, you can remove this part (but no harm leaving it):
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
@@ -34,163 +35,110 @@ uint32_t blueColor = pixels.Color(0, 0, 255);
 uint32_t whiteColor = pixels.Color(255, 255, 255);
 uint32_t orangeColor = pixels.Color(252, 53, 3);
 uint32_t yellowColor = pixels.Color(255, 255, 0);
-void loop() {
 
-  verticalTripleFlag(blueColor, whiteColor, redColor);  // France
+void loop()
+{
+  verticalTripleFlag(blueColor, whiteColor, redColor); // France
   delay(5000);
-  verticalTripleFlag(greenColor, whiteColor, orangeColor);  //irleandFlag();
+  verticalTripleFlag(greenColor, whiteColor, orangeColor); //irleandFlag();
   delay(5000);
   doubleHorizontalFlag(whiteColor, redColor); //polandFlag();
-  delay(5000);  
+  delay(5000);
   czechFlag();
   delay(5000);
-  verticalTripleFlag(greenColor, whiteColor, redColor);  //italyFlag();
+  verticalTripleFlag(greenColor, whiteColor, redColor); //italyFlag();
   delay(5000);
   tripleHorizontalFlag(redColor, whiteColor, greenColor); //hungaryFlag();
   delay(5000);
   doubleHorizontalFlag(blueColor, yellowColor); //ukraineFlag();
-  //Sweden flag
-  lightAll(0, 0, 255);
-  int swed1[] = {0, 0, 0, 1, 0, 0};
-  int swed2[] = {0, 0, 0, 1, 0, 0, 0, 0, 0};
-  lightHorizontalStripes(swed1, 255, 255, 0);
-  lightVerticalStripes(swed2, 255, 255, 0);
+  swedenFlag(); //Sweden flag
   delay(5000);
-  verticalTripleFlag(blueColor, yellowColor, redColor);  //romaniaFlag();
+  verticalTripleFlag(blueColor, yellowColor, redColor); //romaniaFlag();
   delay(5000);
+  denmarkFlag(); // Denmark flag
+  delay(5000);
+  //  pixels.clear(); // Set all pixel colors to 'off'
+}
 
-
-  // Switzerland flag
-  lightAll(255, 0, 0); 
-  int switz1[] = {0, 0, 0, 1, 0, 0};
-  lightHorizontalStripes(switz1, 255, 255, 255);
-  int switz2[] = {0, 0, 0, 1, 0, 0, 0, 0, 0};
-  lightVerticalStripes(switz2, 255, 255, 255);
-  delay(500);
-//  pixels.clear(); // Set all pixel colors to 'off'  
+void doubleHorizontalFlag(uint32_t Color1, uint32_t Color2)
+{
+  for (int i = 0; i < 27; i++)
+  {
+    pixels.setPixelColor(i, Color1);
+    pixels.show(); // Send the updated pixel colors to the hardware.
   }
 
-void doubleHorizontalFlag(uint32_t Color1, uint32_t Color2){
-    for(int i=0; i<27; i++) { 
-        pixels.setPixelColor(i, Color1);
-        pixels.show();   // Send the updated pixel colors to the hardware.
-    }
-
-    for(int i=27; i<55; i++){
-        pixels.setPixelColor(i, Color2);
-        pixels.show();
-    }
-}
-
-void tripleHorizontalFlag(uint32_t Color1, uint32_t Color2, uint32_t Color3){
-    for(int i=0; i<18; i++) { 
-        pixels.setPixelColor(i, Color1);
-        pixels.show();   // Send the updated pixel colors to the hardware.
-    }
-
-    for(int i=18; i<36; i++){
-        pixels.setPixelColor(i, Color2);
-        pixels.show();
-    }
-
-    for(int i=36; i<54; i++){
-        pixels.setPixelColor(i, Color3);
-        pixels.show();
-    }
-}
-
-
-void czechFlag(){
-    for(int i=0; i<27; i++) { // For each pixel...
-
-    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-    // Here we're using a moderately bright green color:
-    pixels.setPixelColor(i, pixels.Color(100, 100, 100));
-    
-    pixels.show();   // Send the updated pixel colors to the hardware.
-}
-
-for(int i=27; i<55; i++){
-    pixels.setPixelColor(i, pixels.Color(252, 0, 0));
+  for (int i = 27; i < 55; i++)
+  {
+    pixels.setPixelColor(i, Color2);
     pixels.show();
+  }
 }
 
+void tripleHorizontalFlag(uint32_t Color1, uint32_t Color2, uint32_t Color3)
+{
+  for (int i = 0; i < 18; i++)
+  {
+    pixels.setPixelColor(i, Color1);
+    pixels.show(); // Send the updated pixel colors to the hardware.
+  }
 
-    for(int i=0; i<7; i++) { 
-    pixels.setPixelColor(i*9, pixels.Color(0, 0, 250));
-    pixels.setPixelColor(i*9+1, pixels.Color(0, 0, 250));
-    pixels.show();   
-}
-
-    pixels.setPixelColor(11, pixels.Color(0, 0, 250));  
-    pixels.setPixelColor(20, pixels.Color(0, 0, 250)); 
-    pixels.setPixelColor(21, pixels.Color(0, 0, 250));  
-    pixels.setPixelColor(29, pixels.Color(0, 0, 250));  
-    pixels.setPixelColor(30, pixels.Color(0, 0, 250));  
-    pixels.setPixelColor(38, pixels.Color(0, 0, 250));
+  for (int i = 18; i < 36; i++)
+  {
+    pixels.setPixelColor(i, Color2);
     pixels.show();
+  }
+
+  for (int i = 36; i < 54; i++)
+  {
+    pixels.setPixelColor(i, Color3);
+    pixels.show();
+  }
 }
 
-
-
-void verticalTripleFlag(uint32_t Color1, uint32_t Color2, uint32_t Color3){
-    for(int i=0; i<7; i++) { // For each pixel...
-
-    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-    // Here we're using a moderately bright green color:
-    pixels.setPixelColor(i*9, Color1);
-    pixels.setPixelColor(i*9+1, Color1);
-    pixels.setPixelColor(i*9+2, Color1);
-    
-    pixels.setPixelColor(i*9+3, Color2);
-    pixels.setPixelColor(i*9+4, Color2);
-    pixels.setPixelColor(i*9+5, Color2);
-
-    pixels.setPixelColor(i*9+6, Color3);
-    pixels.setPixelColor(i*9+7, Color3);
-    pixels.setPixelColor(i*9+8, Color3);
-    
-    pixels.show();   // Send the updated pixel colors to the hardware.
-}}
-
-
-void italyFlag(){
-    for(int i=0; i<7; i++) { // For each pixel...
+void verticalTripleFlag(uint32_t Color1, uint32_t Color2, uint32_t Color3)
+{
+  for (int i = 0; i < 7; i++)
+  { // For each pixel...
 
     // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
     // Here we're using a moderately bright green color:
-    pixels.setPixelColor(i*9, pixels.Color(0, 204, 0));
-    pixels.setPixelColor(i*9+1, pixels.Color(0, 204, 0));
-    pixels.setPixelColor(i*9+2, pixels.Color(0, 204, 0));
-    
-    pixels.setPixelColor(i*9+3, pixels.Color(100, 100, 100));
-    pixels.setPixelColor(i*9+4, pixels.Color(100, 100, 100));
-    pixels.setPixelColor(i*9+5, pixels.Color(100, 100, 100));
+    pixels.setPixelColor(i * 9, Color1);
+    pixels.setPixelColor(i * 9 + 1, Color1);
+    pixels.setPixelColor(i * 9 + 2, Color1);
 
-    pixels.setPixelColor(i*9+6, pixels.Color(250, 0, 0));
-    pixels.setPixelColor(i*9+7, pixels.Color(250, 0, 0));
-    pixels.setPixelColor(i*9+8, pixels.Color(250, 0, 0));
-    
-    pixels.show();   // Send the updated pixel colors to the hardware.
-}}
+    pixels.setPixelColor(i * 9 + 3, Color2);
+    pixels.setPixelColor(i * 9 + 4, Color2);
+    pixels.setPixelColor(i * 9 + 5, Color2);
+
+    pixels.setPixelColor(i * 9 + 6, Color3);
+    pixels.setPixelColor(i * 9 + 7, Color3);
+    pixels.setPixelColor(i * 9 + 8, Color3);
+
+    pixels.show(); // Send the updated pixel colors to the hardware.
+  }
+}
 
 //Poziome pasy
-void lightHorizontalStripes(int tab[], int red_value, int green_value, int blue_value) {
+void lightHorizontalStripes(int tab[], int red_value, int green_value, int blue_value)
+{
 
-  int led_numbers [6] = {0, 9, 18, 27, 36, 45};
+  int led_numbers[6] = {0, 9, 18, 27, 36, 45};
 
-  for (int i = 0; i < 6; i++) {
-    if (tab[i] == 1) {
+  for (int i = 0; i < 6; i++)
+  {
+    if (tab[i] == 1)
+    {
       int n = led_numbers[i];
       pixels.setPixelColor(n, pixels.Color(red_value, green_value, blue_value));
-      pixels.setPixelColor(n+1, pixels.Color(red_value, green_value, blue_value));
-      pixels.setPixelColor(n+2, pixels.Color(red_value, green_value, blue_value));
-      pixels.setPixelColor(n+3, pixels.Color(red_value, green_value, blue_value));
-      pixels.setPixelColor(n+4, pixels.Color(red_value, green_value, blue_value));
-      pixels.setPixelColor(n+5, pixels.Color(red_value, green_value, blue_value));
-      pixels.setPixelColor(n+6, pixels.Color(red_value, green_value, blue_value));
-      pixels.setPixelColor(n+7, pixels.Color(red_value, green_value, blue_value));
-      pixels.setPixelColor(n+8, pixels.Color(red_value, green_value, blue_value));
+      pixels.setPixelColor(n + 1, pixels.Color(red_value, green_value, blue_value));
+      pixels.setPixelColor(n + 2, pixels.Color(red_value, green_value, blue_value));
+      pixels.setPixelColor(n + 3, pixels.Color(red_value, green_value, blue_value));
+      pixels.setPixelColor(n + 4, pixels.Color(red_value, green_value, blue_value));
+      pixels.setPixelColor(n + 5, pixels.Color(red_value, green_value, blue_value));
+      pixels.setPixelColor(n + 6, pixels.Color(red_value, green_value, blue_value));
+      pixels.setPixelColor(n + 7, pixels.Color(red_value, green_value, blue_value));
+      pixels.setPixelColor(n + 8, pixels.Color(red_value, green_value, blue_value));
 
       pixels.show();
     }
@@ -198,27 +146,109 @@ void lightHorizontalStripes(int tab[], int red_value, int green_value, int blue_
 }
 
 //Pionowe pasy
-void lightVerticalStripes(int tab[], int red_value, int green_value, int blue_value) {
+void lightVerticalStripes(int tab[], int red_value, int green_value, int blue_value)
+{
 
-  for (int i = 0; i < 9; i++) {
-    if (tab[i] == 1) {
+  for (int i = 0; i < 9; i++)
+  {
+    if (tab[i] == 1)
+    {
       pixels.setPixelColor(i, pixels.Color(red_value, green_value, blue_value));
-      pixels.setPixelColor(i+9, pixels.Color(red_value, green_value, blue_value));
-      pixels.setPixelColor(i+18, pixels.Color(red_value, green_value, blue_value));
-      pixels.setPixelColor(i+27, pixels.Color(red_value, green_value, blue_value));
-      pixels.setPixelColor(i+36, pixels.Color(red_value, green_value, blue_value));
-      pixels.setPixelColor(i+45, pixels.Color(red_value, green_value, blue_value));
+      pixels.setPixelColor(i + 9, pixels.Color(red_value, green_value, blue_value));
+      pixels.setPixelColor(i + 18, pixels.Color(red_value, green_value, blue_value));
+      pixels.setPixelColor(i + 27, pixels.Color(red_value, green_value, blue_value));
+      pixels.setPixelColor(i + 36, pixels.Color(red_value, green_value, blue_value));
+      pixels.setPixelColor(i + 45, pixels.Color(red_value, green_value, blue_value));
 
       pixels.show();
     }
   }
 }
 
-void lightAll(int red_value, int green_value, int blue_value) {
+void lightAll(int red_value, int green_value, int blue_value)
+{
 
-  for (int i = 0; i < 54; i++) {
+  for (int i = 0; i < 54; i++)
+  {
     pixels.setPixelColor(i, pixels.Color(red_value, green_value, blue_value));
 
     pixels.show();
   }
+}
+
+void czechFlag()
+{
+  for (int i = 0; i < 27; i++)
+  { // For each pixel...
+
+    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+    // Here we're using a moderately bright green color:
+    pixels.setPixelColor(i, pixels.Color(100, 100, 100));
+
+    pixels.show(); // Send the updated pixel colors to the hardware.
+  }
+
+  for (int i = 27; i < 55; i++)
+  {
+    pixels.setPixelColor(i, pixels.Color(252, 0, 0));
+    pixels.show();
+  }
+
+  for (int i = 0; i < 7; i++)
+  {
+    pixels.setPixelColor(i * 9, pixels.Color(0, 0, 250));
+    pixels.setPixelColor(i * 9 + 1, pixels.Color(0, 0, 250));
+    pixels.show();
+  }
+
+  pixels.setPixelColor(11, pixels.Color(0, 0, 250));
+  pixels.setPixelColor(20, pixels.Color(0, 0, 250));
+  pixels.setPixelColor(21, pixels.Color(0, 0, 250));
+  pixels.setPixelColor(29, pixels.Color(0, 0, 250));
+  pixels.setPixelColor(30, pixels.Color(0, 0, 250));
+  pixels.setPixelColor(38, pixels.Color(0, 0, 250));
+  pixels.show();
+}
+
+void italyFlag()
+{
+  for (int i = 0; i < 7; i++)
+  { // For each pixel...
+
+    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+    // Here we're using a moderately bright green color:
+    pixels.setPixelColor(i * 9, pixels.Color(0, 204, 0));
+    pixels.setPixelColor(i * 9 + 1, pixels.Color(0, 204, 0));
+    pixels.setPixelColor(i * 9 + 2, pixels.Color(0, 204, 0));
+
+    pixels.setPixelColor(i * 9 + 3, pixels.Color(100, 100, 100));
+    pixels.setPixelColor(i * 9 + 4, pixels.Color(100, 100, 100));
+    pixels.setPixelColor(i * 9 + 5, pixels.Color(100, 100, 100));
+
+    pixels.setPixelColor(i * 9 + 6, pixels.Color(250, 0, 0));
+    pixels.setPixelColor(i * 9 + 7, pixels.Color(250, 0, 0));
+    pixels.setPixelColor(i * 9 + 8, pixels.Color(250, 0, 0));
+
+    pixels.show(); // Send the updated pixel colors to the hardware.
+  }
+}
+
+void swedenFlag ()
+{
+  lightAll(0, 0, 255);
+  int swed1[] = {0, 0, 0, 1, 0, 0};
+  int swed2[] = {0, 0, 0, 1, 0, 0, 0, 0, 0};
+  lightHorizontalStripes(swed1, 255, 255, 0);
+  lightVerticalStripes(swed2, 255, 255, 0);
+  delay(5000);
+}
+
+void denmarkFlag()
+{
+  lightAll(255, 0, 0);
+  int den1[] = {0, 0, 0, 1, 0, 0};
+  lightHorizontalStripes(den1, 255, 255, 255);
+  int den2[] = {0, 0, 0, 1, 0, 0, 0, 0, 0};
+  lightVerticalStripes(2, 255, 255, 255);
+  delay(500);
 }
